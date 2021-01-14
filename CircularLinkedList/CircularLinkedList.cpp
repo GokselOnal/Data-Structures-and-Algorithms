4ca6cf578154d798b628f3a586940af6493f661f
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-01-14 21:20:11 +0300

    CircularList

--- CircularLinkedList/CircularLinkedList.cpp
+++ CircularLinkedList/CircularLinkedList.cpp
@@ -9,15 +9,15 @@ void CircularList::push(int item){
     Node *iter = root;
     if(root == NULL){
         root = newNode;
-        newNode->next = root;
+        newNode->next = newNode;
         return;
     }
-    newNode->next = root;
-    root = newNode;
     //find tail
      while(iter->next != root){
         iter = iter->next;
     }
+    newNode->next = root;
+    root = newNode;
     iter->next = root;
 }
 
@@ -26,11 +26,11 @@ void CircularList::print(){
     if(root == NULL){
         std::cout << "List is Empty";
     }
+    std::cout << iter->data << " ";
     while(iter->next != root){
-        std::cout << iter->data << " ";
+        std::cout << iter->next->data << " ";
         iter = iter->next;
     }
-    std::cout << iter->data;
 }
 
 void CircularList::append(int item){
@@ -54,10 +54,38 @@ void CircularList::pop(){
         std::cout << "List is empty";
         return;
     }
+    if(getSize() == 1){
+        delete root;
+        root = NULL;
+        return;
+    }
     while(iter->next != root){
         iter = iter->next;
     }
     iter->next = root->next;
     delete root;
     root = iter->next;
+}
+
+int CircularList::getSize(){
+    Node *iter = root;
+    if(root == NULL){
+        return 0;
+    }
+    int count = 1;
+    while(iter->next != root){
+        iter = iter->next;
+        count++;
+    }
+    return count;
+}
+
+CircularList::~CircularList(){
+    Node *iter = root;
+    while(iter->next != root){
+        root = root->next;
+        delete iter;
+        iter = root;
+    }
+    
 }
