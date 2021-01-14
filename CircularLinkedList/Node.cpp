3fbb54a871df23d5d8f498a546a1fc04bdf7df0b
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-01-14 20:00:53 +0300

    Node

---
+++ CircularLinkedList/Node.cpp
@@ -0,0 +1,5 @@
+#include "Node.h"
+
+Node::Node(int item){
+    this->data = item;
+}
---
+++ CircularLinkedList/Node.h
@@ -0,0 +1,12 @@
+#ifndef _NODE_
+#define _NODE_
+#include<iostream>
+
+class Node{
+public:
+    Node(int);
+    int data;
+    Node *next;
+};
+
+#endif
